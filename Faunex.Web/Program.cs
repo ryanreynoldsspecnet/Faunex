c146af27bf22b0772c176d8918927d47606e55ed
ryanreynoldsspecnet <55143521+ryanreynoldsspecnet@users.noreply.github.com>
2025-12-26 03:46:41 +0200

    fixeshttpsredirect

--- Faunex.Web/Program.cs
+++ Faunex.Web/Program.cs
@@ -27,7 +27,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
