e12388a59389db330e14d6016d3b4b83b2439903
ryanreynoldsspecnet <55143521+ryanreynoldsspecnet@users.noreply.github.com>
2025-12-29 10:58:39 +0200

    added migration

---
+++ Faunex.Api/Auth/Migrations/20251229085808_AddDisplayNameToApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Faunex.Api.Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "AspNetUsers");
+        }
+    }
+}
